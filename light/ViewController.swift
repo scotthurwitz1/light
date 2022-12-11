0442e75f55c5e45dfcff9c03f6ff936146ffc443
Scotthurwitz1 <hurwitzscott@gmail.com>
2022-12-10 19:11:13 -0500

    "Successfully created an app that changes the screen from black to white, and back, at the tap of a button—something like a flashlight. Simplified the design of the app as well, making conscious decisions to improve the user experience by removing the button title and filling the screen with a tappable area. Even with limited knowledge of Swift, stepped through lines of code and refer to the documentation to complete this project."

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 }
 
